8e63bbedde8ae32d452f2c04ceb29cb1523ba35c
andreypekar <avpekar@gmail.com>
2021-11-03 20:59:23 +0300

    new method

--- ishop2/MyIShopList.js
+++ ishop2/MyIShopList.js
@@ -4,21 +4,27 @@ var MyIShopList = React.createClass({
 
     render: function() {
   
-      var rowsArr=this.props.listProducts.map( function( item ){
-        return React.DOM.li({key:'li'+item.code},
-          React.DOM.div({key:item.code,className:'Row'},
+      var rowsArr=this.props.listProducts.map( function( item ) {
+        return React.DOM.div({key:item.code,className:'Row'},
             React.DOM.div({className:'Cell'},React.DOM.span({},item.name)),
             React.DOM.div({className:'Cell'},React.DOM.span({},item.price)),
             React.DOM.div({className:'Cell'},React.DOM.span({},item.url)),
             React.DOM.div({className:'Cell'},React.DOM.span({},item.count)),
-            React.DOM.div({className:'Cell'},React.DOM.div({className:'Btn'},React.DOM.span({},React.DOM.input({type:'button',value:'Delete'})))),
-          ),
+            React.DOM.div({className:'Cell'},React.DOM.input({type:'button',value:'Delete'})),
         );
       });
 
       return React.DOM.div( {className:'MyIShopList'},
         React.DOM.h1( {}, this.props.shopName ),
-        React.DOM.ul( {className:'Catalog'}, rowsArr ),
+        React.DOM.div( {className:'ishopTable'},
+          React.DOM.div( {className:'Header'},
+            React.DOM.div({className:'Cell'},'Name'),
+            React.DOM.div({className:'Cell'},'Price'),
+            React.DOM.div({className:'Cell'},'URL'),
+            React.DOM.div({className:'Cell'},'Quantity'),
+            React.DOM.div({className:'Cell'},'Control'),
+          ),
+          rowsArr ),
       );
     },
   

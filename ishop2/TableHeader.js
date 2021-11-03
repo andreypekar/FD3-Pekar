1ed37fa5469a21f30a4f396adb81f7d6eed1dcb1
andreypekar <avpekar@gmail.com>
2021-11-04 01:14:28 +0300

    return Header

--- ishop2/TableHeader.js
+++ ishop2/TableHeader.js
@@ -2,16 +2,7 @@
 
   displayName: 'TableHeader',
 
-  propTypes: {
-    colnames: React.PropTypes.arrayOf(
-      React.PropTypes.shape({
-        col: React.PropTypes.number,
-        name: React.PropTypes.string,
-      })
-    ),
-  },
-
-  getDefaultProps: function () {
+/*   getDefaultProps: function () {
     return {
       colnames: [
         {col:1, name:'Name'},
@@ -19,23 +10,41 @@
         {col:3, name:'URL'},
         {col:4, name:'Quantity'},
         {col:5, name:'Control'}
-      ],
+      ]
     };
-  },
+  }, */
+
+/*   propTypes: {
+    colnames: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        col: React.PropTypes.number,
+        name: React.PropTypes.string,
+      })
+    ),
+  }, */
 
   render: function() {
 
-    var arrHeaderRow = new Array();
+/*     var arrHeaderRow = [];
 
-    for (let i=1; i<=this.props.colnames.length; i++) {
-      for ('col' in this.props.colnames[i]) {
-        if (this.props.colnames[i]['col'] == i) {
-          arrHeaderRow.push(React.DOM.div( {className:'HeaderCell'}, this.props.colnames[i]['name']));
+    for (let i=0; i<this.props.colnames.length; i++) {
+      for (key in this.props.colnames[i]) {
+        if (key == 'col') {
+          if (this.props.colnames[i][key] == (i+1)){
+            arrHeaderRow.push(React.DOM.div( {className:'HeaderCell'}, this.props.colnames[i]['name']));
+          }
         }
       }
-    }
+    } */
 
     return
-      React.DOM.div( {className:'TableHeader'}, arrHeaderRow );
+//      React.DOM.div( {className:'TableHeader'}, arrHeaderRow );
+    React.DOM.div( {className:'TableHeader'},
+      React.DOM.div({className:'HeaderCell'},'Name'),
+      React.DOM.div({className:'HeaderCell'},'Price'),
+      React.DOM.div({className:'HeaderCell'},'URL'),
+      React.DOM.div({className:'HeaderCell'},'Quantity'),
+      React.DOM.div({className:'HeaderCell'},'Control'),
+    );
   },
 });
--- ishop2/ishop.js
+++ ishop2/ishop.js
@@ -1,35 +1,35 @@
 ﻿var IShop = React.createClass({
 
-    displayName: 'IShop',
+  displayName: 'IShop',
 
-    propTypes: {
-        shopName: React.PropTypes.string.isRequired,
-        listProducts: React.PropTypes.arrayOf(
-          React.PropTypes.shape({
-            name: React.PropTypes.string,
-            price: React.PropTypes.number,
-            url: React.PropTypes.string,
-            code: React.PropTypes.number,
-            count: React.PropTypes.number,
-          })
-        ),
-      },
-    
-      getInitialState: function() {
-        return { arrGoods: this.props.listProducts };
-      },
-    
-    render: function() {
+  propTypes: {
+    shopName: React.PropTypes.string.isRequired,
+    listProducts: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        name: React.PropTypes.string,
+        price: React.PropTypes.number,
+        url: React.PropTypes.string,
+        code: React.PropTypes.number,
+        count: React.PropTypes.number,
+      })
+    ),
+  },
+
+  getInitialState: function() {
+    return { arrGoods: this.props.listProducts };
+  },
   
-      var rowsArr=this.state.arrGoods.map( function( item ) {
-        return React.createElement( Goods, {name:item.name,price:item.price,url:item.url,code:item.code,count:item.count});
-      });
+  render: function() {
 
-      return React.DOM.div( {className:'IShop'},
-        React.DOM.h1( {}, this.props.shopName ),
-        React.DOM.div( {className:'Table'},
-          React.createElement( TableHeader, {colnames:[{col:1, name:'Name'},{col:2, name:'Price'},{col:3, name:'URL'},{col:4, name:'Quantity'},{col:5, name:'Control'}]}),
-          rowsArr ),
-      );
-    },
-  });
+    var rowsArr=this.state.arrGoods.map( function( item ) {
+      return React.createElement( Goods, {name:item.name,price:item.price,url:item.url,code:item.code,count:item.count});
+    });
+
+    return React.DOM.div( {className:'IShop'},
+      React.DOM.h1( {}, this.props.shopName ),
+      React.DOM.div( {className:'Table'},
+        React.createElement( TableHeader, {name:'header'}),
+        rowsArr ),
+    );
+  },
+});

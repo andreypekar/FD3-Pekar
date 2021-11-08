1ca850b9c9286d86353009162a69a891f6dad740
andreypekar <avpekar@gmail.com>
2021-11-09 00:46:18 +0300

    separate processing

--- filter/MyFilter.js
+++ filter/MyFilter.js
@@ -9,22 +9,38 @@
   getInitialState: function() {
     return { pattern:'',
       isSorting:false,
+      resStr:this.props.strArray.join('\n'),
+    };
+  },
+
+  processing: function() {
+    var res = this.props.strArray;
+
+    if (this.state.pattern != '') {
+      res = res.filter(el => { if( el.indexOf(this.state.pattern) != -1 ) return true; });
+    };
+
+    if (this.state.isSorting) {
+      this.setState( {resStr:(res.slice()).sort().join('\n')} );
+    }
+    else {
+      this.setState( {resStr:res.join('\n')} );
     };
   },
 
   setPattern: function(EO) {
     console.log( 'сработала setPattern' );
-    this.setState( {pattern:EO.target.value} );
+    this.setState( {pattern:EO.target.value}, this.processing );
   },
 
   setSortFlag: function(EO) {
     console.log( 'сработала setSortFlag' );
-    this.setState( {isSorting:EO.target.checked} );
+    this.setState( {isSorting:EO.target.checked}, this.processing );
   },
 
   allReset: function() {
     console.log( 'сработала allReset' );
-    this.setState( {pattern:'', isSorting:false} );
+    this.setState( {pattern:'', isSorting:false}, this.processing );
   },
 
   render: function() {
@@ -40,7 +56,7 @@
       React.DOM.div( {className:'dTable'},
         React.DOM.div( {className:'dRow', id:'uRow'},
           React.DOM.div( {className:'dCell', id:'uCell'},
-            React.createElement(MyList,{strArr:this.props.strArray, patternText:this.state.pattern, isSortFlag:this.state.isSorting}),
+            React.createElement(MyList,{strArr:this.state.resStr, patternText:this.state.pattern, isSortFlag:this.state.isSorting}),
           ),
         ),
       ),
--- filter/MyList.js
+++ filter/MyList.js
@@ -3,14 +3,14 @@
   displayName: 'MyList',
 
   propTypes: {
-    strArr: React.PropTypes.arrayOf(React.PropTypes.string),
+    strArr: React.PropTypes.string,
     patternText: React.PropTypes.string,
     isSortFlag: React.PropTypes.bool,
   },
 
   render: function() {
 
-    function makeStr(arrStr, strPattern) {
+/*     function makeStr(arrStr, strPattern) {
       return function(flgSort) {
         var arrWP=arrStr.filter(el => { if( el.indexOf(strPattern) != -1 ) return true; });
 
@@ -18,8 +18,8 @@
       }
     };
 
-    var fnGetStrFromArray = makeStr(this.props.strArr,this.props.patternText);
+    var fnGetStrFromArray = makeStr(this.props.strArr,this.props.patternText); */
 
-    return React.DOM.textarea({name:'List', id:'MyList', rows:10, readOnly:'true', value:fnGetStrFromArray(this.props.isSortFlag)});
+    return React.DOM.textarea( {name:'List', id:'MyList', rows:10, readOnly:'true', value:this.props.strArr} );
   },
 });

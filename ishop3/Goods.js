953a52eca2b4e59b6b4eaffb5b4b93e37d13e014
andreypekar <avpekar@gmail.com>
2021-11-18 23:16:46 +0300

    add npm

---
+++ ishop3/Goods.js
@@ -0,0 +1,37 @@
+﻿var Goods = React.createClass({
+
+  displayName: 'Goods',
+
+  propTypes: {
+    name: React.PropTypes.string,
+    price: React.PropTypes.number,
+    url: React.PropTypes.string,
+    code: React.PropTypes.number,
+    count: React.PropTypes.number,
+    cbSelected: React.PropTypes.func,
+    cbDeleted: React.PropTypes.func,
+    isSelectRow: React.PropTypes.bool,
+  },
+
+  rowClicked: function(EO) {
+    if (EO.defaultPrevented) return;
+    console.log( 'выбрана строка # '+this.props.code + ', сейчас флаг ' + this.props.isSelectRow + ', сейчас подсветка ' + (this.props.isSelectRow?"LightGreen":"transparent") );
+    this.props.cbSelected(this.props.code);
+  },
+
+  rowDeleteClick: function(EO) {
+    EO.preventDefault();
+    console.log( 'выбрана для удаления строка # '+this.props.code );
+    this.props.cbDeleted(this.props.code);
+  },
+  
+  render: function() {
+    return React.DOM.div({className:'GoodsRow',onClick:this.rowClicked, style:(this.props.isSelectRow?{backgroundColor:'LightGreen'}:{}),},
+      React.DOM.div({className:'GoodsCell'},React.DOM.span({},this.props.name)),
+      React.DOM.div({className:'GoodsCell'},React.DOM.span({},this.props.price)),
+      React.DOM.div({className:'GoodsCell'},React.DOM.span({},this.props.url)),
+      React.DOM.div({className:'GoodsCell'},React.DOM.span({},this.props.count)),
+      React.DOM.div({className:'GoodsCell'},React.DOM.input({type:'button',value:'Delete',onClick:this.rowDeleteClick})),
+    );
+  },
+});
---
+++ ishop3/TableHeader.js
@@ -0,0 +1,42 @@
+﻿var TableHeader = React.createClass({
+
+  displayName: 'TableHeader',
+
+  getDefaultProps: function () {
+    return {
+      colnames: [
+        {col:1, name:'Name'},
+        {col:2, name:'Price'},
+        {col:3, name:'URL'},
+        {col:4, name:'Quantity'},
+        {col:5, name:'Control'}
+      ]
+    };
+  },
+
+  propTypes: {
+    colnames: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        col: React.PropTypes.number,
+        name: React.PropTypes.string,
+      })
+    ),
+  },
+
+  render: function() {
+
+    var arrHeaderRow = [];
+
+    for (let i=0; i<this.props.colnames.length; i++) {
+      for (key in this.props.colnames[i]) {
+        if (key == 'col') {
+          if (this.props.colnames[i][key] == (i+1)){
+            arrHeaderRow.push(React.DOM.div( {key:this.props.colnames[i][key],className:'HeaderCell'}, this.props.colnames[i]['name']));
+          }
+        }
+      }
+    }
+
+    return React.DOM.div( {className:'TableHeader'}, arrHeaderRow );
+  },
+});

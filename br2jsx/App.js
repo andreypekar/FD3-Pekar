c92fec0087ce8eace214548e8bb3ac58c64b29f1
andreypekar <avpekar@gmail.com>
2021-12-06 02:20:02 +0300

    br2jsx finall

---
+++ br2jsx/App.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import BR2JSX from './components/br2jsx';
+
+let text="первый<br>второй<br/>третий<br />последний";
+let myElem = <BR2JSX text={text}/>;
+
+ReactDOM.render(
+  myElem
+  ,document.getElementById('container')
+);
---
+++ br2jsx/components/br2jsx.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from 'react';
+
+import './br2jsx.css';
+
+const BR2JSX = props => {
+  let re = /(<.*?>)/;
+  let br = <br/>;
+
+  return <div className='br2jsx'>{props.text.split(re).map((item) => (item[0] != '<' ? item : br))}</div>;
+};
+
+export default BR2JSX;

940d84caaa79cbf0fe2730b167b7ef604d2bf81f
andreypekar <avpekar@gmail.com>
2021-12-06 02:55:33 +0300

    RainbowFrame final

---
+++ RainbowFrame/components/ColorFrame.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+const ColorFrame = props => (
+      <div style={{border:"solid 5px "+props.color,padding:"10px"}}>
+        {props.children}
+      </div>
+);
+
+ColorFrame.propTypes = {
+  color: PropTypes.string.isRequired,
+};
+
+export default ColorFrame;
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+import React, { Children } from 'react';
+import './RainbowFrame.css';
+import ColorFrame from './ColorFrame';
+
+const RainbowFrame = props => {
+
+  let arrJSX=null;
+
+  props.colors.forEach( elem => 
+    {
+      arrJSX = (arrJSX == null ? <ColorFrame color={elem}>{props.children}</ColorFrame> : <ColorFrame color={elem}>{arrJSX}</ColorFrame>);
+    }
+  );
+
+  return <div className='RainbowFrame'>{arrJSX}</div>;
+};
+
+export default RainbowFrame;

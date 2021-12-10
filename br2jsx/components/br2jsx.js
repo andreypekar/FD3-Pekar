8a0076211ad1d48db0f08775e80f641e3b5228de
andreypekar <avpekar@gmail.com>
2021-12-10 15:43:03 +0300

    br2jsx разные варианты

--- br2jsx/components/br2jsx.js
+++ br2jsx/components/br2jsx.js
@@ -5,14 +5,17 @@ import React from 'react';
 import './br2jsx.css';
 
 const BR2JSX = props => {
-//  let re = /(<.*?>)/;
-  let re = /(<br\s*\/?>)/i;
-  let br = <br/>;
-  let indexFlag = true;
+//  let re = /(<.*?>)/; // любой тег <>
+  let re = /(<br\s*\/?>)/i; // только <br< ,/>> с любыми вариацими
+  let indexFlag = true; // флаг чётности индексов
 
-  return <div className='br2jsx'>{props.text.split(re).map((item) => { 
-    indexFlag = !indexFlag;
-    if (indexFlag) return br;
+  return <div className='br2jsx'>{props.text.split(re).map((item, i) => { 
+/*
+    if ((i+1)%2 === 0) return <br key={i}/>; // можно через остаток от деления
+    else return item;
+*/
+    indexFlag = !indexFlag; // можно просто флаг чётности менять
+    if (indexFlag) return <br key={i}/>;
     else return item;
   })}</div>;
 };

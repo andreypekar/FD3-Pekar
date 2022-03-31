03eeac50ebe749f502fdac1a1d28f1dc9048ec16
avpekar <avpekar@gmail.com>
2022-03-31 12:10:28 +0300

    final

---
+++ closureTest/closureTest.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+
+function showHelp(help) {
+  document.getElementById('help').innerHTML = help;
+}
+
+function setupHelp() {
+  var helpText = [
+      {'id': 'email', 'help': 'Ваш адрес e-mail'},
+      {'id': 'name', 'help': 'Ваше полное имя'},
+      {'id': 'age', 'help': 'Ваш возраст (Вам должно быть больше 16)'}
+    ];
+
+  for (var i = 0; i < helpText.length; i++) {
+    let item = helpText[i]; //var item - будет работать неправильно, всегда последний элемент хэша про возраст будет отображаться
+    document.getElementById(item.id).onfocus = function() {
+//    document.getElementById(item.id).onmouseover = function() {
+        showHelp(item.help);
+    }
+  }
+}
+
+setupHelp();
